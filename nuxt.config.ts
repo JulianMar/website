9ffba629bc6e9f12118217a2fe15e4276d207b3c
Julian Martin <julian.martin@russmedia.com>
2023-02-07 14:23:18 +0100

    feat: add bugsnag update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,21 @@
-import { defineNuxtConfig } from "nuxt";
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ["@/assets/css/main.css"],
+  css: ['@/assets/css/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  modules: ['nuxt-bugsnag'],
+  bugsnag: {
+    publishRelease: true,
+    baseUrl: 'https://julian-martin.com',
+    config: {
+      apiKey: '317c3d7013a3dc4a9e152138bfe8c900',
+      enabledReleaseStages: ['staging', 'production'],
+      releaseStage: process.env.NODE_ENV,
+      appVersion: '1.1',
     },
   },
 });
